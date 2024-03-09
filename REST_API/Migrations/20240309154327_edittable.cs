efa2f82e45bbeff90bdd8fa90c0b58eb447a9f07
Jannat Akter <jfkhatun021@gmail.com>
2024-03-09 22:12:15 +0600

    edit

---
+++ REST_API/Migrations/20240309154327_edittable.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace REST_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class edittable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Questions_ArticleId",
+                table: "Questions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Answers_QuestionId",
+                table: "Answers");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_ArticleId",
+                table: "Questions",
+                column: "ArticleId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Answers_QuestionId",
+                table: "Answers",
+                column: "QuestionId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Questions_ArticleId",
+                table: "Questions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Answers_QuestionId",
+                table: "Answers");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_ArticleId",
+                table: "Questions",
+                column: "ArticleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Answers_QuestionId",
+                table: "Answers",
+                column: "QuestionId");
+        }
+    }
+}
