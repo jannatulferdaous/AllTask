7950764633e6f29b8b3d04587d37cb7d7971d33e
Jannat Akter <jfkhatun021@gmail.com>
2024-05-20 17:46:41 +0600

    AjaxCall

---
+++ QuizGame/Views/Shared/articleForm.js
@@ -0,0 +1,47 @@
+﻿
+$(document).ready(function () {
+    $("#submit-btn").click(function () {
+        var language = $("#language").val();
+        var title = $("#title").val();
+        var articleText = $("#articleText").val();
+        var isValid = true;
+        var errorMessage = "";
+        if (language.length < 1) {
+            isValid = false;
+            $("#language-error").html("Language is required.");
+        } else {
+            $("#language-error").html(""); // Clear error message if language is valid
+        }
+        if (title.length < 3) {
+            isValid = false;
+            $("#title-error").html("Title must be at least 3 characters long."); // Set specific error message
+        } else {
+            $("#title-error").html("");
+        }
+        if (articleText.length < 50) {
+            isValid = false;
+            $("#articleText-error").html("Article text must be at least 50 characters long.");
+        } else {
+            $("#articleText-error").html("");
+        }
+        tinyMCE.triggerSave();
+
+        if (!isValid) {
+            return false;
+        }
+        $.ajax({
+            url: '/Article/Create',
+            type: 'POST',
+            data: $("#articleForm").serialize(),
+            success: function (response) {
+                console.log('Article created successfully!');
+
+            },
+            error: function (xhr, status, error) {
+                console.error('Error creating article:', error);
+
+            }
+        });
+        return false;
+    });
+});
--- REST_API/obj/Debug/net8.0/REST_API.AssemblyInfo.cs
+++ REST_API/obj/Debug/net8.0/REST_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("REST_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfb958cba7d4b10818f3cf6a978f2993dcd39a09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b45d41f5193eaa87807b1426854d27d2e4e8617")]
 [assembly: System.Reflection.AssemblyProductAttribute("REST_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("REST_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
